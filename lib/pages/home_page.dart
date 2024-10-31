c83695c8ef1e496104f4f965ed41430b963845ab
ibrahimbinbuga <ibrahimm3334@icloud.com>
2024-10-31 12:37:47 +0300

    logout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,10 +3,23 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
+
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   final user = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
       body: Center(
         child: Text("Logged in as: " + user.email!),
       ),
